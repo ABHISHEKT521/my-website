45950f4a80cf7453007abc922e4ce996b7c3d3c5
ABHISHEKT521 <abhishek26371@gmail.com>
2024-07-25 11:20:25 +0530

    Update script.js
    Signed-off-by: ABHISHEKT521 <abhishek26371@gmail.com>

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 const checkbox=document.getElementById("slider")
-checkbox.addEventListener('change',()=>{
+checkbox.addEventListener('click',()=>{
     document.body.classList.toggle('dark');
 });
+
+var datedis=document.getElementById("time")
+
+setInterval(getresult,1000)
+function getresult(){
+    var currentdatetime= new Date();
+
+
+var chour = appendZero(currentdatetime.getHours());
+var cmin = appendZero(currentdatetime.getMinutes());
+var csec = appendZero(currentdatetime.getSeconds());
+
+var result =chour + ":" + cmin + ":" + csec ;
+datedis.innerText =result;
+
+}
+
+function appendZero(value){
+    return(value<10) ? '0'+ value:value;
+      
+}
